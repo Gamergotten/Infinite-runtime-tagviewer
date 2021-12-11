551c2c78aeddeefc78aa28e2ad541fab8bbe85f6
Callum Carmicheal <callumcarmicheal@gmail.com>
2021-12-11 04:37:59 +0000

    Added Dark Theme, Fixed Popup & Changed StackPanel to VirtualizingStackPanel

---
+++ Extension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assembly69
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// Gets the window left.
+        /// </summary>
+        /// <param name="window">The window.</param>
+        /// <returns></returns>
+        public static double GetWindowLeft(this Window window)
+        {
+            if (window.WindowState == WindowState.Maximized)
+            {
+                var leftField = typeof(Window).GetField("_actualLeft", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                return (double) leftField.GetValue(window);
+            }
+            else
+                return window.Left;
+        }
+
+        /// <summary>
+        /// Gets the window top.
+        /// </summary>
+        /// <param name="window">The window.</param>
+        /// <returns></returns>
+        public static double GetWindowTop(this Window window)
+        {
+            if (window.WindowState == WindowState.Maximized)
+            {
+                var topField = typeof(Window).GetField("_actualTop", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                return (double) topField.GetValue(window);
+            }
+            else
+                return window.Top;
+        }
+    }
+}
