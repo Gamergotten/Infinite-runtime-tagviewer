63c5d590ad9a26f4a023ff72dafb4d21c50b251f
Callum Carmicheal <callumcarmicheal@gmail.com>
2021-12-11 07:01:24 +0000

    Added Dark Theme title bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+
 using Assembly69.Interface.Controls;
 using Assembly69.Interface.Windows;
 using AvalonDock.Layout;
@@ -39,6 +41,8 @@ namespace Assembly69
         public MainWindow()
         {
             InitializeComponent();
+            StateChanged += MainWindowStateChangeRaised;
+
 
             inhale_tagnames();
         }
@@ -506,5 +510,55 @@ namespace Assembly69
          * TagrefGroup
          * TagrefTag
          */
+
+        #region Window Styling
+
+        // Can execute
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        // Maximize
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        // Restore
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        // State change
+        private void MainWindowStateChangeRaised(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(8);
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
+            }
+        }
+        #endregion
     }
 }
