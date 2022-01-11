cc0122a21f089b646e14f188a1e55f96aed1137a
xxZxx <36254276+Z-15@users.noreply.github.com>
2022-01-10 23:30:16 -0600

    RGB Block Files

---
+++ Interface/Controls/TagRGBBlock.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace InfiniteRuntimeTagViewer.Interface.Controls
+{
+    /// <summary>
+    /// Interaction logic for valueBlock.xaml
+    /// </summary>
+    public partial class TagRGBBlock
+    {
+        public TagRGBBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
