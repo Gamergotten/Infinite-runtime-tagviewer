a65b48fadee53a84ffcddc60fc9023dbd4fa9efd
xxZxx <36254276+Z-15@users.noreply.github.com>
2022-01-14 19:35:17 -0600

    Update details in description
    Flag Group Name Update:
     - TagFlagsGroup.xaml: Updated UI
     - TagEditorControl.xaml.cs: Added tfg.flag_name.Text = fg.N
    
    Tag Two Block: Bounds and 2D Points
     - TagTwoBlock.xaml: Created UI
     - TagTwoBlock.xaml.cs: Initialized
     - Added BoundsFloat, Bounds2Byte, 2DPoint_Float, and 2DPoint_2Byte cases in TagEditorControl.cs
     - Updated cases for _10, _16, _18, _1B, _23, _24, _25, _26 in TagLayout.cs
    
    Tag Three Block: 3D Points
     - TagThreeBlock.xaml: Created UI
     - TagThreeBlock.xaml.cs: Initialized
     - Added 3DPoint case in TagEditorControl.cs
     - Updated cases for _1C, _19, _17 in TagLayout.cs
    
    Tag Four Block: Quaternion and Plane
     - TagFourBlock.xaml: Created UI
     - TagFourBlock.xaml.cs: Initialized
     - Added 3DPlane and Quaternion in TagEditorControl.cs
     - Updated cases for _1A and _1E in TagLayout.cs

---
+++ Interface/Controls/TagThreeBlock.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace InfiniteRuntimeTagViewer.Interface.Controls
+{
+    /// <summary>
+    /// Interaction logic for valueBlock.xaml
+    /// </summary>
+    public partial class TagThreeBlock
+    {
+		public TagThreeBlock()
+        {
+            InitializeComponent();
+        }
+	}
+}
