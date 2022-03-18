c3eb8e93f57ea4836a7fa9f803edcfe0d6183c44
Sopitive <30811278+Sopitive@users.noreply.github.com>
2022-03-17 20:59:52 -0600

    Fix WindowChrome issues on mod window.

--- Interface/Windows/ModWindow.xaml.cs
+++ Interface/Windows/ModWindow.xaml.cs
@@ -279,5 +279,52 @@ namespace InfiniteRuntimeTagViewer.Interface.Windows
 			debug_text.Text = "Queue cleared";
 			load_mods_from_directories();
 		}
+
+		// Can execute
+		private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		// Minimize
+		private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.MinimizeWindow(this);
+		}
+
+		// Maximize
+		private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.MaximizeWindow(this);
+		}
+
+		// Restore
+		private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.RestoreWindow(this);
+		}
+
+		// Close
+		private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.CloseWindow(this);
+		}
+
+		// State change
+		private void MainWindowStateChangeRaised(object? sender, EventArgs e)
+		{
+			if (WindowState == WindowState.Maximized)
+			{
+				MainWindowBorder.BorderThickness = new Thickness(8);
+				RestoreButton.Visibility = Visibility.Visible;
+				MaximizeButton.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				MainWindowBorder.BorderThickness = new Thickness(0);
+				RestoreButton.Visibility = Visibility.Collapsed;
+				MaximizeButton.Visibility = Visibility.Visible;
+			}
+		}
 	}
 }
