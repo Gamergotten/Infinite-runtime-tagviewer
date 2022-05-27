c4ca71c805a1bece53b1a18845e43358629fc6e2
xxZxx <NotARealEmail@gmail.com>
2022-05-26 19:09:36 -0500

    Adds database version to save files. Will be used for future updates to tag structs.
    Not my code, just adding it for someone else.

--- Interface/Windows/Savewindow.xaml.cs
+++ Interface/Windows/Savewindow.xaml.cs
@@ -38,32 +38,41 @@ namespace InfiniteRuntimeTagViewer.Interface.Windows
 			extrastufftoappend = all_the_poke_text;
 		}
 
+
+		public string database_version = "DB_V2"; // this will only need to be updated when tag structures are altered by 343
+
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			// SAVE
 			string dump = "";
 			string destination = base_path + "\\" + NAME_FIELD.Text + ".irtv";
-			if (DESC_FIELD.Text == "" && filters_panel.Children.Count == 0)
-			{
-				System.IO.File.WriteAllText(destination, extrastufftoappend);
-			}
-			else
-			{
-				dump = "^";	
-				foreach (var pee in filters_panel.Children)
-				{
-					TextBox tb = pee as TextBox;
-					if (tb.Text != "" && tb.Text != null && !tb.Text.Contains("~"))
-					{
-						dump += tb.Text + "~";
-					}
-				}
-				dump += "\r\n"+DESC_FIELD.Text + "\r\n";
-				dump += "^\r\n";
-				dump += extrastufftoappend;
-				System.IO.File.WriteAllText(destination, dump);
 
+			// it is quite possible that we can get away with just commenting all that out
+			//if (DESC_FIELD.Text == "" && filters_panel.Children.Count == 0)
+			//{
+			//    System.IO.File.WriteAllText(destination, extrastufftoappend);
+			//}
+			//else
+			//{
+			dump = "^";
+			foreach (var pee in filters_panel.Children)
+			{
+				TextBox tb = pee as TextBox;
+				if (tb.Text != "" && tb.Text != null && !tb.Text.Contains("~"))
+				{
+					dump += tb.Text + "~";
+				}
 			}
+			dump += "\r\n" + DESC_FIELD.Text + "\r\n";
+			dump += "^" + database_version + "\r\n"; // <----- here is where we could slot our database info into 
+			//#NOTE: the IRTV internal mod manager may not like us having text on the same line after the "^", so bewarned
+			// as far as i'm aware this "^" has no purpose other than to tell the internal mod manager that that is the end the of mod's description
+			// you may have to split the string in the internal mod manager to check for the "^" as i likely hooked it up to read a line at a time and checks for a line that just returns the "^", idk
+
+			dump += extrastufftoappend;
+			System.IO.File.WriteAllText(destination, dump);
+
+			//}
 			if (copycheck.IsChecked == true)
 			{
 				string fileNameWithExt = System.IO.Path.GetFileName(destination);
